b8f8a41ff4df0f6546796977105cb84c83cf32c5
HARINI SREE <146451708+HARINISREE85@users.noreply.github.com>
2025-10-30 22:08:16 +0530

    push

---
+++ Day19/Task5.js
@@ -0,0 +1,5 @@
+//Skip the number 7 using continue in a loop.
+for(let i=1;i<=10;i++){
+    if(i==7)continue;
+    console.log(i);   
+}
---
+++ Day19/Task8.js
@@ -0,0 +1,7 @@
+//Display all odd numbers between 1–50 using while.
+for(let i=1;i<=50;i++)
+{
+   if (i%2==0) {
+    console.log(i);  
+   } 
+}
